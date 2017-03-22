47617ed5bf7600deedda359fb9ded8497e1368a3
Svatok <s_s_s@ua.fm>
2017-03-22 11:47:23 +0200

    Update set_address.rb

--- app/commands/set_address.rb
+++ app/commands/set_address.rb
@@ -2,22 +2,42 @@ class SetAddress < Rectify::Command
   def initialize(options)
     @params = options[:params]
     @object = options[:object]
-    @address_forms = {}
+    set_address_forms
   end
 
+  def create
+    addresses_params['address_forms'].each do |address_type, address_params|
+      address_form = instance_variable_set("@#{address_type}_address_form", UserAddressForm.from_params(address_params))
+      next unless params[address_type] == 'true'
+      if address_form.valid?
+        @address = current_user.addresses.find_by(id: address_params['id'])
+        @address = current_user.addresses.new unless @address.present?
+        @address.attributes = address_form.attributes.except('id')
+        @address.save
+      else
+        form_with_errors = true
+      end
+    end
+    return render :template => 'devise/registrations/edit',
+                      :locals => {
+                        :resource => current_user,
+                        :resource_name => 'user' } if form_with_errors
+    flash[:success] = "Address has been updated."
+    redirect_to edit_user_registration_path
+  end
+  
   def call
-    set_addresses_forms
     @address_forms.each do |address_type, address_form|
+      next unless @params[address_type] == 'true'
       return broadcast(:invalid, @address_forms) unless address_form.valid?
-      @address = @object.addresses.find_or_initialize_by(address_type: address_type)
-      @address.update_attributes(address_form.attributes)
+      set_address
     end
     broadcast(:ok)
   end
 
   private
 
-  def set_addresses_forms
+  def set_address_forms
     @params['address_forms'].each do |address_type, params|
       @address_forms[address_type.to_sym] = UserAddressForm.from_params(address_params(params))
     end
@@ -32,6 +52,11 @@ class SetAddress < Rectify::Command
 
   def use_billing?
     @params['use_billing'] == 'on' && @address_forms[:billing].present?
+  end
+
+  def set_address
+    @address = @object.addresses.find_or_initialize_by(address_type: address_type)
+    @address.update_attributes(address_form.attributes)
   end
 
   def permit_params(params)
