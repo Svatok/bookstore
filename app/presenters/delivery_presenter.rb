b79e13cc743d685ddaef12a020435e834d1cc179
Svatok <s_s_s@ua.fm>
2017-03-22 17:17:22 +0200

    Update delivery_presenter.rb

--- app/presenters/delivery_presenter.rb
+++ app/presenters/delivery_presenter.rb
@@ -1,12 +1,11 @@
 class DeliveryPresenter < Rectify::Presenter
   attribute :object
-  attribute :new_order_shipping, OrderItem, :default => current_order.order_items.new
 
   def available_shippings
     Product.shippings.decorate
   end
   
   def current_order_shipping
-    object.payments.presenr? ? object.payments.first : new_order_shipping
+    object.shippings.present? ? object.shippings.first : object.order_items.new
   end 
 end
