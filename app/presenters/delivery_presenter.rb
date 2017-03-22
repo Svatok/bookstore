688f1dc682ff21d7499da4a2dd3841ac5446434c
Svatok <s_s_s@ua.fm>
2017-03-22 14:09:43 +0200

    Create delivery_presenter.rb

---
+++ app/presenters/delivery_presenter.rb
@@ -0,0 +1,25 @@
+class DeliveryPresenter < Rectify::Presenter
+  attribute :objects
+  attribute :new_address, UserAddressForm, :default => UserAddressForm.new
+
+  def address_form(address_type)
+    return form_with_errors(address_type) if forms_has_errors?
+    form_without_errors(address_type)
+  end
+  
+  private
+  
+  def form_with_errors(address_type)
+    objects[address_type.to_sym]
+  end
+  
+  def forms_has_errors?
+    objects.is_a?(Hash)
+  end
+
+  def form_without_errors(address_type)
+    address = @objects.find_by(address_type: address_type) || current_user.addresses.find_by(address_type: address_type)
+    address.present? ? UserAddressForm.from_model(address) : new_address
+  end
+  
+end
