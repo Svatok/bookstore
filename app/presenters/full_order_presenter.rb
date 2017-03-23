3219c1d449912044ed3624d2fc5224de964aaf8c
Svatok <s_s_s@ua.fm>
2017-03-23 09:49:18 +0200

    Update full_order_presenter.rb

--- app/presenters/full_order_presenter.rb
+++ app/presenters/full_order_presenter.rb
@@ -1,17 +1,19 @@
 class FullOrderPresenter < Rectify::Presenter
   attribute :object
   
-  def current_payment_form
-    form_has_errors? ? object : form_without_errors
-  end
-  
-  private
-  
-  def form_has_errors?
-    object.is_a?(PaymentForm)
+  def address(address_type)
+    object.addresses.send('address_type').first.decorate
   end
 
-  def form_without_errors
-    object.payments.present? ? PaymentForm.from_model(object.payments.first) : new_payment
+  def shipping
+    object.shippings.first
+  end
+
+  def payment
+    object.payments.first.decorate
+  end
+  
+  def order_items
+    object.order_items.only_products.decorate
   end
 end
