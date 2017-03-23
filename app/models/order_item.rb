97b25744e5ac71f77a3c76a62d8a98ce77ab89d8
Svatok <s_s_s@ua.fm>
2017-03-23 17:36:39 +0200

    Update order_item.rb

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,43 +5,34 @@ class OrderItem < ApplicationRecord
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validate :product_present, :order_present
   before_save :finalize, :set_inactive_for_coupon
-  after_save :update_order_price
   before_destroy :set_active_for_coupon
   scope :only_products, -> { joins(:product).merge(Product.main) }
   scope :only_shippings, -> { joins(:product).merge(Product.shippings) }
   scope :only_coupons, -> { joins(:product).merge(Product.coupons) }
 
-   def total_price
-     unit_price * quantity
-   end
+  def total_price
+    unit_price * quantity
+  end
 
- private
-  
- def product_present
-   if product.nil?
-     errors.add(:product, "is not valid or is not active.")
-   end
- end
+  private
 
- def order_present
-   if order.nil?
-     errors.add(:order, "is not a valid order.")
-   end
- end
+  def product_present
+   errors.add(:product, "is not valid or is not active.") if product.nil?
+  end
 
- def finalize
+  def order_present
+   errors.add(:order, "is not a valid order.") if order.nil?
+  end
+
+  def finalize
    self[:unit_price] = product.prices.actual.first.value
- end
+  end
 
- def set_inactive_for_coupon
+  def set_inactive_for_coupon
    self.product.update_attributes(status: 'inactive') if self.product.product_type == 'coupon'
- end
+  end
 
- def set_active_for_coupon
+  def set_active_for_coupon
    self.product.update_attributes(status: 'active') if self.product.product_type == 'coupon'
- end
-  
-  def update_order_price
-    order.update_total_price!
-  end 
+  end
 end
