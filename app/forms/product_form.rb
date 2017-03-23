269a4a4a30a4b62050abce8dbdf5a3cef1648835
Svatok <s_s_s@ua.fm>
2017-03-23 17:18:26 +0200

    Update product_form.rb

--- app/forms/product_form.rb
+++ app/forms/product_form.rb
@@ -20,35 +20,41 @@ class ProductForm < Rectify::Form
   validates :stock, presence: true, numericality: { greater_than_or_equal_to: 0 }, if: :is_product?
   validate :validate_characteristics
 
+  def save
+    return false unless valid?
+    persist!
+    true
+  end
+
+  def persist!
+    @product = Product.find_by_id(id).present? ? Product.find_by_id(id) : Product.new
+    @product.update_attributes(title: title, description: description, category_id: category, status: status, product_type: product_type)
+    add_price
+    add_stock
+    add_authors
+    add_characteristics
+    add_pictures
+  end
+  
+  private
+  
   def validate_characteristics
     return unless characteristics.present?
     characteristics.each do |characteristic|
       errors.add(:characteristics, :invalid) unless characteristic['value'].present? && characteristic['property_id'].present?
     end
   end
-
-  def save
-    if valid?
-      persist!
-      true
-    else
-      false
-    end
+  
+  def add_price
+    current_price = @product.prices.present? ? @product.prices.actual.first.value : 0.0
+    @product.prices.create(value: price, date_start: Date.today.to_s) unless current_price == price.to_f    
   end
 
-  def persist!
-    @product = Product.find_by_id(id).present? ? Product.find_by_id(id) : Product.new
-    @product.update_attributes(title: title, description: description, category_id: category, status: status, product_type: product_type)
-    @product.save
-    current_price = @product.prices.present? ? @product.prices.actual.first.value : 0.0
-    @product.prices.create(value: price, date_start: Date.today.to_s) unless current_price == price.to_f
+  def add_stock
     current_stock = @product.stocks.present? ? @product.stocks.actual.first.value : 0
     @product.stocks.create(value: stock, date_start: Date.today.to_s) unless current_stock == stock.to_i
-    add_authors
-    add_characteristics
-    add_pictures
   end
-
+  
   def is_product?
     product_type == 'product'
   end
@@ -67,8 +73,7 @@ class ProductForm < Rectify::Form
     characteristics.each do |value|
       next if value['value'] == ''
       characteristic = @product.characteristics.find_or_initialize_by(property_id: value['property_id'])
-      characteristic.value = value['value']
-      characteristic.save
+      characteristic.update_attributes(value: value['value'])
     end
   end
 
@@ -78,6 +83,4 @@ class ProductForm < Rectify::Form
       @product.pictures.create(image_path: picture)
     end
   end
-
-
 end
