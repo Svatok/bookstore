f0bc1630ac23d7f82ac037b5946f7961e7719d26
Svatok <s_s_s@ua.fm>
2017-03-23 16:23:13 +0200

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,12 @@
 class ReviewsController < ApplicationController
 
   def create
-    @user = current_user || User.new
-    @product = Product.find(params[:product_review][:product_id]).decorate
-    @reviews = @product.reviews.approved.decorate
-    @review_form = ProductReviewForm.from_params(params)
-    @order_item = current_order.order_items.new
-    if @review_form.valid?
-      Review.create(@review_form.attributes)
-      flash[:success] = 'Thanks for Review. It will be published as soon as Admin will approve it.'
-      redirect_to product_path(@product)
-    else
-      render :template => 'products/show'
-    end
+    review_form = ProductReviewForm.from_params(params)
+    present ProductPresenter.new(review_form: review_form)
+    return render :template => 'products/show' unless review_form.valid?
+    Review.create(review_form.attributes)
+    flash[:success] = 'Thanks for Review. It will be published as soon as Admin will approve it.'
+    redirect_to product_path(@product)
   end
 
 end
