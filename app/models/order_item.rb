ae019a4f6c2abe54daaccb8ab6485700deda78d5
Svatok <s_s_s@ua.fm>
2017-03-23 14:06:45 +0200

    Update order_item.rb

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,45 +3,45 @@ class OrderItem < ApplicationRecord
   belongs_to :order
 
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validate :product_present
-  validate :order_present
+  validate :product_present, :order_present
   before_save :finalize, :set_inactive_for_coupon
+  after_save :update_order_price
   before_destroy :set_active_for_coupon
   scope :only_products, -> { joins(:product).merge(Product.main) }
   scope :only_shippings, -> { joins(:product).merge(Product.shippings) }
   scope :only_coupons, -> { joins(:product).merge(Product.coupons) }
-
-  def unit_price
-    product.prices.actual.first.value
-  end
 
    def total_price
      unit_price * quantity
    end
 
  private
-   def product_present
-     if product.nil?
-       errors.add(:product, "is not valid or is not active.")
-     end
+  
+ def product_present
+   if product.nil?
+     errors.add(:product, "is not valid or is not active.")
    end
+ end
 
-   def order_present
-     if order.nil?
-       errors.add(:order, "is not a valid order.")
-     end
+ def order_present
+   if order.nil?
+     errors.add(:order, "is not a valid order.")
    end
+ end
 
-   def finalize
-     self[:unit_price] = unit_price
-   end
+ def finalize
+   self[:unit_price] = product.prices.actual.first.value
+ end
 
-   def set_inactive_for_coupon
-     self.product.update_attributes(status: 'inactive') if self.product.product_type == 'coupon'
-   end
+ def set_inactive_for_coupon
+   self.product.update_attributes(status: 'inactive') if self.product.product_type == 'coupon'
+ end
 
-   def set_active_for_coupon
-     self.product.update_attributes(status: 'active') if self.product.product_type == 'coupon'
-   end
-
-  end
+ def set_active_for_coupon
+   self.product.update_attributes(status: 'active') if self.product.product_type == 'coupon'
+ end
+  
+  def update_order_price
+    order.update_total_price!
+  end 
+end
