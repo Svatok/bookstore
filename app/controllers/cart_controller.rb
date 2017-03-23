42c14e587d2d3d906d15e034c83098df35c1d734
Svatok <s_s_s@ua.fm>
2017-03-23 13:10:19 +0200

    Update cart_controller.rb

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -8,32 +8,35 @@ class CartController < ApplicationController
   end
 
   def update
-    params[:order_items] = [] if params[:coupon_only].present?
-    @order = current_order
-    params[:order_items].each do |order_item_id, order_item_params|
-      @order_item = @order.order_items.find(order_item_id)
-      next unless @order_item.present?
-      @order_item.update_attributes(quantity: order_item_params[:quantity])
-      @order.save
+    UpdateOrderItems.call({ object: current_user, params: params }) do
+      on(:ok) { |msg| flash[:error] = msg }
+      on(:invalid) { |msg| flash[:error] = msg }
     end
     redirect_back(fallback_location: root_path)
+#     return if params[:coupon_only].present?
+#     params[:order_items].each do |order_item_id, order_item_params|
+#       @order_item = @order.order_items.find(order_item_id)
+#       next unless @order_item.present?
+#       @order_item.update_attributes(quantity: order_item_params[:quantity])
+#       @order.save
+#     end
   end
 
-  private
+#   private
 
-    def coupon_add
-      code = params[:coupon][:code]
-      return unless code.present?
-      coupon = Product.coupons.find_by(title: code, status: 'active')
-      return unless coupon.present?
-      previous_coupon_delete
-      current_order.order_items.create(product_id: coupon.id, quantity: 1)
-    end
+#     def coupon_add
+#       code = params[:coupon][:code]
+#       return unless code.present?
+#       coupon = Product.coupons.find_by(title: code, status: 'active')
+#       return unless coupon.present?
+#       previous_coupon_delete
+#       current_order.order_items.create(product_id: coupon.id, quantity: 1)
+#     end
 
-    def previous_coupon_delete
-      coupon = current_order.order_items.select{ |order_item| order_item.product.product_type == 'coupon' }
-      return unless coupon.present?
-      coupon_item = current_order.order_items.find(coupon.first.id)
-      coupon_item.destroy
-    end
+#     def previous_coupon_delete
+#       coupon = current_order.order_items.select{ |order_item| order_item.product.product_type == 'coupon' }
+#       return unless coupon.present?
+#       coupon_item = current_order.order_items.find(coupon.first.id)
+#       coupon_item.destroy
+#     end
 end
