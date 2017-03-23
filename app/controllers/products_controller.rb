00ab759cb6f05a4ac50427e958c19dbf92956d0a
Svatok <s_s_s@ua.fm>
2017-03-23 16:24:57 +0200

    Update products_controller.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,17 +8,7 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @user = current_user || User.new
-    @product = Product.find(params[:id]).decorate
-    @reviews = @product.reviews.approved.decorate
-    @review_form = ProductReviewForm.new
-    @order_item = current_order.order_items.new
-  end
-
-  private
-
-  def order_item_params
-    params.require(:order_item).permit(:quantity, :product_id)
+    present ProductPresenter.new(product: Product.find(params[:id]).decorate)
   end
 
 end
