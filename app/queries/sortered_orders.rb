a5fe5dd541113836e1e8415260ba22b0a84fa3a5
Svatok <s_s_s@ua.fm>
2017-03-23 18:12:36 +0200

    Update sortered_orders.rb

--- app/queries/sortered_orders.rb
+++ app/queries/sortered_orders.rb
@@ -1,4 +1,5 @@
 class SorteredOrders < Rectify::Query
+  helper PaginationHelper
 
   def initialize(orders, params)
     @params = params
@@ -11,44 +12,4 @@ class SorteredOrders < Rectify::Query
   def query
     @orders.where(state: @params[:sort]).limit(@limit).offset(offset)
   end
-
-  def current_page
-    @page
-  end
-
-  def total_pages
-    total = query.count
-    (total.to_f / @limit).ceil
-  end
-
-  def limit_value
-    @limit
-  end
-
-  def offset
-    (@page - 1) * @limit
-  end
-
-  def prev_page
-    current_page - 1 unless first_page? || out_of_range?
-  end
-
-  def next_page
-    current_page + 1 unless last_page? || out_of_range?
-  end
-
-
-  private
-  def first_page?
-    current_page == 1
-  end
-
-  def last_page?
-    current_page == total_pages
-  end
-
-  def out_of_range?
-    current_page > total_pages
-  end
-
 end
