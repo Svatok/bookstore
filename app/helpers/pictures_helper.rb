e9e4d502f6045ddb040ecd618d5b96f87ca7fb56
Svatok <s_s_s@ua.fm>
2017-03-24 15:59:07 +0200

    Update pictures_helper.rb

--- app/helpers/pictures_helper.rb
+++ app/helpers/pictures_helper.rb
@@ -2,8 +2,8 @@ module PicturesHelper
 
   def avatar_tag(user_id, user_name, style)
     capture_haml do
-      return haml_tag('span.' + style + '.logo-empty', user_name.first) unless user_avatar(user_id).present?
-      haml_tag('img.' + style, src: user_avatar(user_id).image_path)
+      haml_tag('span.' + style + '.logo-empty', user_name.first) unless user_avatar(user_id).present?
+      haml_tag('img.' + style, src: user_avatar(user_id).image_path.to_s) if user_avatar(user_id).present?
     end
   end
 
