8872fb70bca0a3a87b4c389fe03e964eabb4316e
Svatok <s_s_s@ua.fm>
2017-03-27 14:58:54 +0300

    for pr without assets

---
+++ app/models/address.rb
@@ -0,0 +1,7 @@
+class Address < ApplicationRecord
+  belongs_to :addressable, polymorphic: true
+  belongs_to :country
+  scope :billing, -> { where("address_type = 'billing'").limit(1) }
+  scope :shipping, -> { where("address_type = 'shipping'").limit(1) }
+
+end
